cb521c87afbbe3a2f21009f48accd9646f662a65
Rassul <rasa.laik@gmail.com>
2024-12-09 01:58:49 +0500

    lab 2 end term

---
+++ src/test/java/source/lab2_endterm/Lab2EndtermApplicationTests.java
@@ -0,0 +1,13 @@
+package source.lab2_endterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2EndtermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
